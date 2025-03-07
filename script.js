254d39d8e7a1b0f3fdd55161f8c4b4fd3d226478
Igor110717 <igorkotov110717@gmail.com>
2025-03-07 13:50:09 +0200

    делает 6, 7,8,9,10 зазания

--- script.js
+++ script.js
@@ -64,3 +64,72 @@ console.table(users);
 //   console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 //   console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
   
+// Задание 6
+
+// const getUsersWithAge = (users, min, max) => {
+//     const filteredUsers = users. filter(function(user) {
+//         return user.age >= min && user.age <= max;
+//     })
+//     return filteredUsers
+//   };
+//   console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+// console.log(getUsersWithAge(users, 30, 40));
+// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+
+// Задание 7
+
+// const calculateTotalBalance = users => {
+//   const totalBalance = users.reduce((sum, user) => {
+//     return sum + user.balance;
+//   },0)
+//   return totalBalance
+// };
+// console.log(calculateTotalBalance(users)); // 20916
+
+
+// Задание 8
+
+// const getUsersWithFriend = (users, friendName) => {
+//     const userWithFriend = users.filter (user => {
+//         return user.friends.includes(friendName);
+//     }).map(user => {
+//         return user.name;
+//     })
+//     return userWithFriend
+//   };
+//   console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+
+// Задание 9
+
+// const getNamesSortedByFriendsCount = users => {
+
+//     const sortedUsersByFriendsCount = [...users].sort(function(prevPlayer, nextPlayer){
+//         return prevPlayer.friends.length - nextPlayer.friends.length
+//     }).map(user => {
+   
+//         return user.name;
+   
+//     })
+//     return sortedUsersByFriendsCount
+//   };
+//   console.log(getNamesSortedByFriendsCount(users));
+// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+
+// Задание 10
+
+// const getSortedUniqueSkills = users => {
+//       const sortedSkills = users.reduce(function (skills, user) {
+//         skills.push(...user.skills)
+//     return skills;
+//     },[]). filter ((skill, index, array) => {
+
+// return array.indexOf(skill) === index
+//     }).sort() 
+//         return sortedSkills;
+//   };
+//   console.log(getSortedUniqueSkills(users));
